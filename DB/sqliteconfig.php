1a800716d4385cd2dda8d0f6f47a454d46162704
fedorax <fedorax@hotmail.co.jp>
2014-08-07 08:22:43 +0900

    update sqliteconfig

--- DB/sqliteconfig.php
+++ DB/sqliteconfig.php
@@ -10,13 +10,45 @@ class SQLiteConfig implements IDBConfig
 	/**
 	 * DBファイル
 	 */
-	private $file = NULL;
+	private $_file = NULL;
 	
 	/**
 	 * 暗号キー
 	 */
-	private $key = NULL;
+	private $_key = NULL;
 	
+	/**
+	 * コンストラクタ
+	 *
+	 * @param file SQLite ファイル
+	 * @param key 暗号化キー
+	 *
+	 */
+	public function __construct($file, $key = NULL)
+	{
+		$this->_file = $file;
+		$this->_key = $key;
+	}
 	
+	/**
+	 * 接続文字列を返します.
+	 *
+	 * @return 接続文字列
+	 */
+	public function getConnectionString()
+	{
+		if(!isset($_file))
+		{
+			throw new DBUtilException("Not Set SQLite File.");
+		}
+		$str = "FILE=".$_file;
+		
+		if(isset($_key))
+		{
+			$str .= "&KEY=".$_key;
+		}
+		
+		return $str;
+	}
 }
 ?>
