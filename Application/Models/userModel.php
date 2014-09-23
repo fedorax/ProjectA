517fcc7b67f32dad463d03d46c477f11cb8995ea
fedorax <fedorax@hotmail.co.jp>
2014-09-24 03:10:18 +0900

    2014/09/24 ユーザのデータとmodelを追加

---
+++ Application/Models/userModel.php
@@ -0,0 +1,19 @@
+<?php
+class UserModel
+{
+	public $user_id;
+	public $user_name;
+	public $password;
+	public $enable_flag;
+
+	public $login_datetime;
+
+	public $delete_flag;
+
+	public $create_datetime;
+	public $create_user;
+	public $update_datetime;
+	public $update_user;
+	public $update_num;
+}
+?>
