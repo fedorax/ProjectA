59430834189f85833f42ae1a605173dcb58b0529
fedorax <webmaster@mail.fedorax.info>
2015-04-26 15:23:54 +0900

    ナビゲーションバーのメニューを左にカスタマイズ

--- Application/Views/Parts/navbar.php
+++ Application/Views/Parts/navbar.php
@@ -3,15 +3,22 @@ function setNavBar($title)
 {
 ?>
     <!-- Fixed navbar -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="navbar navbar-inverse navbar-fixed-top custom-navbar" role="navigation">
       <div class="container">
         <div class="navbar-header">
+<?php
+	if(LoginManager::isLogin())
+	{
+?>
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+<?php
+	}
+?>
           <a class="navbar-brand" href="main"><?php echo $title; ?></a>
         </div>
 <?php
